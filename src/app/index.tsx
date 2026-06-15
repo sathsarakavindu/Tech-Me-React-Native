682609c8ee35c6fd5b0733d418414b37b2796fab
Kavindu Sathsara <sathsarakavindu70@gmail.com>
2026-06-15 23:14:49 +0530

    Async storage data save

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,60 +1,89 @@
-import { useRouter } from "expo-router";
-import { Button, StyleSheet, View } from "react-native";
+import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+
 
 export default function Index() {
-  const router = useRouter();
+  
+  const[loading, setLoading] = useState(true);
+  const[isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(()=>{
+     const checkAuth = async()=>{
+      
+     }
+  },[]);
 
   return (
     <View style={styles.container}>
-      <Button title="Go to Login" onPress={() => router.push("/auth/login")} />
-      <Button
-        title="Go to Register"
-        onPress={() => router.push("/auth/register")}
-      />
-      <Button
-        title="Go to Forgot Password"
-        onPress={() => router.push("/auth/forgot_password")}
-      />
-      <Button
-        title="Go to OTP Page"
-        onPress={() => router.push("/auth/otp_page")}
-      />
-      <Button
-        title="Go to Create New Password"
-        onPress={() => router.push("/auth/create_new_password_page")}
-      />
-
-      <Button
-        title="Go to User Dashboard"
-        onPress={() => router.push("/screens/user/user_dashboard_page")}
-      />
-
-      <Button
-        title="Go to User Account"
-        onPress={() => router.push("/screens/user/user_account")}
-      />
-
-      <Button
-        title="Go to Technician Dashboard"
-        onPress={() => router.push("/screens/technician/technician_dashboard")}
-      />
-      <Button
-        title="Go to Technician History"
-        onPress={() => router.push("/screens/technician/technician_history")}
-      />
-
-      <Button
-        title="Go to Add Vehicle"
-        onPress={() => router.push("/screens/user/add_vehicle")}
-      />
+      <Redirect href={"/auth/login"} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    flex: 1
   }
 });
+
+// import { useRouter } from "expo-router";
+// import { Button, StyleSheet, View } from "react-native";
+
+// export default function Index() {
+//   const router = useRouter();
+
+//   return (
+//     <View style={styles.container}>
+//       <Button title="Go to Login" onPress={() => router.push("/auth/login")} />
+//       <Button
+//         title="Go to Register"
+//         onPress={() => router.push("/auth/register")}
+//       />
+//       <Button
+//         title="Go to Forgot Password"
+//         onPress={() => router.push("/auth/forgot_password")}
+//       />
+//       <Button
+//         title="Go to OTP Page"
+//         onPress={() => router.push("/auth/otp_page")}
+//       />
+//       <Button
+//         title="Go to Create New Password"
+//         onPress={() => router.push("/auth/create_new_password_page")}
+//       />
+
+//       <Button
+//         title="Go to User Dashboard"
+//         onPress={() => router.push("/screens/user/user_dashboard_page")}
+//       />
+
+//       <Button
+//         title="Go to User Account"
+//         onPress={() => router.push("/screens/user/user_account")}
+//       />
+
+//       <Button
+//         title="Go to Technician Dashboard"
+//         onPress={() => router.push("/screens/technician/technician_dashboard")}
+//       />
+//       <Button
+//         title="Go to Technician History"
+//         onPress={() => router.push("/screens/technician/technician_history")}
+//       />
+
+//       <Button
+//         title="Go to Add Vehicle"
+//         onPress={() => router.push("/screens/user/add_vehicle")}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center"
+//   }
+// });
